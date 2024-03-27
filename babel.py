062fc0bcd296d8c2af85c0323c5afb0ad35d5a99
Hari Koneru <hkoneru@workergpu138.cm.cluster>
2024-03-27 02:32:06 -0400

    get babel to coords and labels complete

--- babel.py
+++ babel.py
@@ -6,25 +6,15 @@ from human_body_prior.body_model.body_model import BodyModel
 import torch
 import numpy as np
 import json
+from tqdm import tqdm
 
 babel = []
 babel.append(json.load(open(HUMAN_DATA_PATH + "BABEL/train.json")))
 babel.append(json.load(open(HUMAN_DATA_PATH + "BABEL/val.json")))
 babel.append(json.load(open(HUMAN_DATA_PATH + "BABEL/test.json")))
 
-babelfull = babel[0] | babel[1] | babel[2]
-keys = [i for i in babelfull.keys()]
+splits = ["train", "val", "test"]
 
-# data = np.load(HUMAN_DATA_PATH + "BABEL/data.npy", allow_pickle=True)
-# labels = np.load(HUMAN_DATA_PATH + "BABEL/labels.npy", allow_pickle=True)
-# meta = np.load(HUMAN_DATA_PATH + "BABEL/meta.npy", allow_pickle=True)
-
-from tqdm import tqdm
-
-meta = []
-data = []
-labels = []
-currframe = 0
 allsets = [
     "ACCAD",
     "BMLmovi",
@@ -47,79 +37,89 @@ allsets = [
 bm_fname = HUMAN_DATA_PATH + "babelmodel.npz"
 num_betas = 16  # number of body parameters
 
-for k in tqdm(keys):
-    # import pdb;pdb.set_trace();
-    # if int(k)>50:
-    #     continue
-    if babelfull[k]["frame_ann"] == None:
-        labels.append(
-            babelfull[k]["seq_ann"]["labels"][0]["act_cat"]
-        )  # gives list of act labels if no frame_ann
-    else:
-        seq = [
-            [i["act_cat"], i["start_t"], i["end_t"]]
-            for i in babelfull[k]["frame_ann"]["labels"]
-        ]  # these 3 lines give ordered list of frame labels for lists of actions
-        sorter = np.array([i[1:] for i in seq])
-        sortedseq = [seq[i] for i in np.argsort(sorter[:, 0])]
-        labels.append(sortedseq)
-    # import pdb;pdb.set_trace();
-    evalset = []
-    for i in babel:
-        evalset.append(k in i.keys())
-    meta.append(
-        [allsets.index(babelfull[k]["feat_p"].split("/")[0]), evalset.index(True), k]
-    )  # dataset ID, evaluation set, babel id, url
 
-    amass_npz_fname = HUMAN_DATA_PATH + "AMASS/" + babelfull[k]["feat_p"]
-    bdata = np.load(amass_npz_fname)
-    time_length = len(bdata["trans"])
+for set in range(len(babel)):
+    meta = []
+    data = []
+    labels = []
+    for k in tqdm(babel[set].keys()):
+        meta.append(
+            [
+                k,
+                # allsets.index(babel[set][k]["feat_p"].split("/")[0]),
+                babel[set][k]["feat_p"].split("/")[0],
+                babel[set][k]["url"],
+            ]
+        )  # babel id, dataset ID, url
 
-    body_parms = {
-        "root_orient": torch.Tensor(
-            bdata["poses"][:, :3]
-        ),  # controls the global root orientation
-        "pose_body": torch.Tensor(bdata["poses"][:, 3:66]),  # controls the body
-        "pose_hand": torch.Tensor(
-            bdata["poses"][:, 66:]
-        ),  # controls the finger articulation
-        "trans": torch.Tensor(bdata["trans"]),  # controls the global body position
-        "betas": torch.Tensor(
-            np.repeat(
-                bdata["betas"][:num_betas][np.newaxis], repeats=time_length, axis=0
-            )
-        ),
-    }
+        amass_npz_fname = HUMAN_DATA_PATH + "AMASS/" + babel[set][k]["feat_p"]
+        bdata = np.load(amass_npz_fname)
+        time_length = len(bdata["trans"])
 
-    bm = BodyModel(bm_fname=bm_fname, num_betas=num_betas, model_type="smplh")
+        body_parms = {
+            "root_orient": torch.Tensor(
+                bdata["poses"][:, :3]
+            ),  # controls the global root orientation
+            "pose_body": torch.Tensor(bdata["poses"][:, 3:66]),  # controls the body
+            "pose_hand": torch.Tensor(
+                bdata["poses"][:, 66:]
+            ),  # controls the finger articulation
+            "trans": torch.Tensor(bdata["trans"]),  # controls the global body position
+            "betas": torch.Tensor(
+                np.repeat(
+                    bdata["betas"][:num_betas][np.newaxis], repeats=time_length, axis=0
+                )
+            ),
+        }
 
-    bm.root_orient = torch.nn.Parameter(body_parms["root_orient"])
-    bm.pose_body = torch.nn.Parameter(body_parms["pose_body"])
-    bm.betas = torch.nn.Parameter(body_parms["betas"])
-    bm.pose_hand = torch.nn.Parameter(body_parms["pose_hand"])
+        bm = BodyModel(bm_path=bm_fname, num_betas=num_betas, model_type="smplh")
 
-    body_pose_beta = bm(
-        **{k: v for k, v in body_parms.items() if k in ["pose_body", "betas"]}
+        bm.root_orient = torch.nn.Parameter(body_parms["root_orient"])
+        bm.pose_body = torch.nn.Parameter(body_parms["pose_body"])
+        bm.betas = torch.nn.Parameter(body_parms["betas"])
+        bm.pose_hand = torch.nn.Parameter(body_parms["pose_hand"])
+
+        body_pose_beta = bm(
+            **{k: v for k, v in body_parms.items() if k in ["pose_body", "betas"]}
+        )
+
+        data.append(np.array(body_pose_beta.Jtr.detach()))
+
+        if babel[set][k]["frame_ann"] == None:
+            labels.append(
+                babel[set][k]["seq_ann"]["labels"][0]["act_cat"]
+            )  # gives list of act labels if no frame_ann
+        else:
+            seq = [
+                [i["act_cat"], i["start_t"], i["end_t"]]
+                for i in babel[set][k]["frame_ann"]["labels"]
+            ]  # these 3 lines give ordered list of frame labels for lists of actions
+            sorter = np.array([i[1:] for i in seq])
+            sortedseq = [seq[i] for i in np.argsort(sorter[:, 0])]
+            framelabels = [[] for i in range(len(data[-1]))]
+            for segment in sortedseq:
+                for i in range(
+                    int(segment[1] * 120), min(int(segment[2] * 120), len(framelabels))
+                ):
+                    framelabels[i] += segment[0]
+            labels.append(framelabels)
+
+    np.save(
+        HUMAN_DATA_PATH + "BABEL/data_" + splits[set] + ".npy",
+        np.asarray(data, dtype="object"),
+        allow_pickle=True,
+    )
+    np.save(
+        HUMAN_DATA_PATH + "BABEL/meta_" + splits[set] + ".npy",
+        np.asarray(meta, dtype="object"),
+        allow_pickle=True,
+    )
+    np.save(
+        HUMAN_DATA_PATH + "BABEL/labels_" + splits[set] + ".npy",
+        np.asarray(labels, dtype="object"),
+        allow_pickle=True,
     )
 
-    data.append(np.array(body_pose_beta.Jtr.detach()))
-
-import pdb
-
-pdb.set_trace()
-
-np.save(
-    HUMAN_DATA_PATH + "BABEL/data.npy",
-    np.asarray(data, dtype="object"),
-    allow_pickle=True,
-)
-np.save(
-    HUMAN_DATA_PATH + "BABEL/meta.npy",
-    np.asarray(meta, dtype="object"),
-    allow_pickle=True,
-)
-np.save(
-    HUMAN_DATA_PATH + "BABEL/labels.npy",
-    np.asarray(labels, dtype="object"),
-    allow_pickle=True,
-)
+# data = np.load(HUMAN_DATA_PATH + "BABEL/data.npy", allow_pickle=True)
+# labels = np.load(HUMAN_DATA_PATH + "BABEL/labels.npy", allow_pickle=True)
+# meta = np.load(HUMAN_DATA_PATH + "BABEL/meta.npy", allow_pickle=True)
